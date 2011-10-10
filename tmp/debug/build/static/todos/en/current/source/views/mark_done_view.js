26190a9b855a758d609e78bb799c21d75b36a970
wschwarz <wschwarz@ucla.edu>
2011-10-09 21:10:27 -0700

    removed junk

--- tmp/debug/build/static/todos/en/current/source/views/mark_done_view.js
+++
@@ -1,4 +0,0 @@
-Todos.MarkDoneView = SC.Checkbox.extend({
-	titleBinding: '.parentView.content.title',
-	valueBinding: '.parentView.content.isDone'
-});; if ((typeof SC !== 'undefined') && SC && SC.Module && SC.Module.scriptDidLoad) SC.Module.scriptDidLoad('todos');
--- tmp/debug/staging/static/todos/en/current/tests/framework_test.js
+++
@@ -1,6 +0,0 @@
-//just to make sure testing framework is working
-module('Framework Check');
-
-test('This should pass', function() {
-    ok(true);
-});
